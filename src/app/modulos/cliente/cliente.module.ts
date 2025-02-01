718d80bf5acc937a85d79764afe08295d596e50e
ygejhegjheg123 <fernandojdsajld@gmail.com>
2025-01-31 23:38:37 -0600

    avanzes de cliente

--- src/app/modulos/cliente/cliente.module.ts
+++ src/app/modulos/cliente/cliente.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { CUSTOM_ELEMENTS_SCHEMA,NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { provideClientHydration } from '@angular/platform-browser';
 import { ClienteRoutingModule } from './cliente-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { ClienteComponent } from './cliente.component';
-
-
+import { ApiService } from '../../services/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     FooterComponent,
@@ -16,10 +18,16 @@ import { ClienteComponent } from './cliente.component';
     InicioComponent,
     CatalogoComponent,
     ClienteComponent,
+    
+    
   ],
   imports: [
     CommonModule,
-    ClienteRoutingModule
+    HttpClientModule, 
+    ClienteRoutingModule,
+    ReactiveFormsModule,FormsModule,
   ]
+  ,schemas:[CUSTOM_ELEMENTS_SCHEMA]
+    ,providers:[provideClientHydration(),ApiService]
 })
 export class ClienteModule { }
