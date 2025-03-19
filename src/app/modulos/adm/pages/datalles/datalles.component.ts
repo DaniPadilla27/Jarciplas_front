4e6fc6dfa57c9fbe77efaf13629a7ae2604e2daa
ygejhegjheg123 <fernandojdsajld@gmail.com>
2025-03-19 09:00:54 -0600

    cambios

---
+++ src/app/modulos/adm/pages/datalles/datalles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datalles',
+  standalone: false,
+  
+  templateUrl: './datalles.component.html',
+  styles: ``
+})
+export class DatallesComponent {
+
+}
+  
---
+++ src/app/modulos/adm/pages/pronostico/pronostico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pronostico',
+  standalone: false,
+  
+  templateUrl: './pronostico.component.html',
+  styles: ``
+})
+export class PronosticoComponent {
+
+}
