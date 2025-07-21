c96f7a9397af305e3bec4694b5e0371629f2bf75
PADILLA <paca23919@gmail.com>
2025-07-21 02:27:59 -0600

    correcion

--- src/app/modulos/cliente/cliente-routing.module.ts
+++ src/app/modulos/cliente/cliente-routing.module.ts
@@ -1,86 +1,67 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import path from 'path';
-import { ClienteComponent } from './cliente.component';
-import { CatalogoComponent } from './pages/catalogo/catalogo.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
-import { OfertasComponent } from './pages/ofertas/ofertas.component';
-import { NotificacionesComponent } from '../adm/pages/notificaciones/notificaciones.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { HistorialComponent } from './pages/historial/historial.component';
-import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
-import { CarritoComponent } from './pages/carrito/carrito.component';
+import { NgModule } from "@angular/core"
+import { RouterModule, type Routes } from "@angular/router"
+import { ClienteComponent } from "./cliente.component"
+import { CatalogoComponent } from "./pages/catalogo/catalogo.component"
+import { InicioComponent } from "./pages/inicio/inicio.component"
+import { OfertasComponent } from "./pages/ofertas/ofertas.component"
+import { NotificacionesComponent } from "../adm/pages/notificaciones/notificaciones.component"
+import { PerfilComponent } from "./pages/perfil/perfil.component"
+import { HistorialComponent } from "./pages/historial/historial.component"
+import { DetalleProductoComponent } from "./pages/detalle-producto/detalle-producto.component"
+import { CarritoComponent } from "./pages/carrito/carrito.component"
 
 const routes: Routes = [
-
-
   {
-    path: '',
-    redirectTo:'home',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
   {
-    path: '',
+    path: "",
     component: ClienteComponent,
     children: [
-
       {
-        path: 'home',
-        component:InicioComponent,
+        path: "home",
+        component: InicioComponent,
       },
       {
-        path: 'catalogo',
-        component:CatalogoComponent,
+        path: "catalogo",
+        component: CatalogoComponent,
       },
       {
-        path: 'ofertas',
-        component:OfertasComponent,
+        path: "ofertas",
+        component: OfertasComponent,
       },
       {
-        path: 'notificaciones',
-        component:NotificacionesComponent,
+        path: "notificaciones",
+        component: NotificacionesComponent,
       },
       {
-        path: 'perfil',
-        component:PerfilComponent,
+        path: "perfil",
+        component: PerfilComponent,
       },
       {
-        path: 'historial',
-        component:HistorialComponent,
+        path: "historial",
+        component: HistorialComponent,
       },
-      // {
-      //   path: 'producto/:id',
-      //   component:DetalleProductoComponent,
-      // },
       {
-  path: 'detalle/:id',
-  component: DetalleProductoComponent,
-  data: {
-    renderMode: 'ssr'  // 👈 Agrega esto
-  }
-},
-
+        // CAMBIO PRINCIPAL: Solo SSR, sin prerendering
+        path: "detalle/:id",
+        component: DetalleProductoComponent,
+        data: {
+          renderMode: "csr", // Cambiar a client-side rendering
+        },
+      },
       {
-        path: 'carrito',
-        component:CarritoComponent,
-      }
-
-
-
-
-
+        path: "carrito",
+        component: CarritoComponent,
+      },
     ],
-  }
-
-
-
-];
-
-
-
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ClienteRoutingModule { }
+export class ClienteRoutingModule {}
