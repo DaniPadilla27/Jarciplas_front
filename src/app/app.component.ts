1391cb67d16409896cbec726ef7ea47c79ff99f4
PADILLA <paca23919@gmail.com>
2025-11-19 10:39:35 -0600

    pwaw2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports: [RouterOutlet,CommonModule],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'JarciplasFront';
+
+  ngOnInit() {
+    this.solicitarPermisosNotificaciones();
+    this.detectarEstadoRed();
+  }
+
+  solicitarPermisosNotificaciones() {
+    if ('Notification' in window) {
+      Notification.requestPermission();
+    }
+  }
+
+  detectarEstadoRed() {
+    window.addEventListener('offline', () => {
+      this.mostrarNotificacion("⚠ Sin conexión", "Te has quedado sin internet.");
+    });
+
+    window.addEventListener('online', () => {
+      this.mostrarNotificacion("✅ Conexión restablecida", "Ya tienes internet nuevamente.");
+    });
+  }
+
+  mostrarNotificacion(titulo: string, cuerpo: string) {
+    if (Notification.permission === 'granted') {
+      new Notification(titulo, { body: cuerpo });
+    }
+  }
 }
